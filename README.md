76522c6bf3ef416e5acaf57c04e4ead9853520e2
OmKharade <133861833+OmKharade@users.noreply.github.com>
2023-10-21 23:30:17 -0400

    Added del, ac, +/-, screen, more than 1 operation

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # calculator
 
 ### (10/20) Core Functionality Implemented
----
 - click digits (string) -> append them to make number 
 - first number is made when operator is clicked
 - second number is made when equals is clicked
 - send them in operation() 
 - console log all actions
+---
+### (10/21) Added remaining buttons, multiple operations functionality
+(explain)
+- del, AC, +/-
+- screen elements
+- successive operations
--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 /* 
- - click digits (string) -> append them to make number 
- - first number is made when operator is clicked
- - second number is made when equals is clicked
- - send them in operation() 
+ - Functionality
+    1. Decimal
+    2. AC and Delete
+    3. Rework to perform multiple operations
+        -> 'perform operation' when =, typeof x is Number
+        ->  after performing result = x
 */
 
-let x = 0
-let y = 0
-// listen for digit 
+let x = null
+let y = null
+// listen for digit and decimal
+let hasDecimal = false 
 const digitBtns = document.querySelectorAll(".digit")
 digitBtns.forEach((button)=>{
     button.addEventListener('click',()=>{
-        makeNumber(button.textContent)
+        if(!hasDecimal || button.textContent != '.' ) // does not have decimal (allows one dot) OR digit 
+            makeNumber(button.textContent)
+        if(button.textContent === '.' )
+            hasDecimal= true
     })
 })
 
+// screen elements
+const screenCurrent = document.querySelector('.screen-current')
+const screenOperator = document.querySelector('.screen-operator')
+const screenPast = document.querySelector('.screen-past')
 // listen for operator and equals
 let operator = ''
 const operatorBtns = document.querySelectorAll('.operator')
@@ -23,24 +33,27 @@ operatorBtns.forEach((button)=>{
         if(button.classList.contains('equals')){ // '='
             y = Number(numberString) // second number is made
             numberString = '' // numberString is reset
-            
-            // perform operation
-            if(operator === '+')
-                console.log(x,operator,y,'=',add(x,y))
-            else if(operator === '-')
-                console.log(x,operator,y,'=',subtract(x,y))
-            else if(operator === '*')
-                console.log(x,operator,y,'=',multiply(x,y))
-            else if(operator === '/')
-                console.log(x,operator,y,'=',divide(x,y))
-            else if(operator === 'xy')
-                console.log(x,'^',y,'=',power(x,y))
+            x = operation(x,operator,y)
+            screenCurrent.textContent = ''
+            screenOperator.textContent = ''
         }
         else{ // '+-*/'
             operator = button.textContent
             console.log(operator);
-            x = Number(numberString) // first number is made
-            numberString = '' // numberString is reset
+            if(operator === '+/-')
+                numberString = '-'+numberString
+            screenCurrent.innerHTML = ''
+            screenOperator.innerHTML = operator
+            screenPast.innerHTML = numberString
+            if(x===null){
+                x = Number(numberString) // first number is made
+                numberString = '' // numberString is reset
+            }
+            else if(y===null){
+                y = Number(numberString) // second number is made
+                numberString = '' // numberString is reset
+                x = operation(x,operator,y)
+            }
         }
     })
 })
@@ -50,7 +63,25 @@ let numberString = ''
 function makeNumber(digit){
     numberString = numberString + digit
     console.log(numberString);
+    screenCurrent.innerHTML = numberString
 }
+
+//listen for deletes
+const deleteBtns = document.querySelectorAll('.delete')
+deleteBtns.forEach((deleteBtn)=>{
+    deleteBtn.addEventListener('click',()=>{
+        if(deleteBtn.classList.contains('all')){ // reset all variables
+            numberString = ''
+            operator = ''
+            x = 0
+            y = 0
+        }
+        else{
+        numberString = numberString.slice(0,-1) // slice last character
+        console.log(numberString)
+        }
+    })
+})
 
 function add(x, y){
     return x+y
@@ -70,5 +101,24 @@ function divide(x, y){
 
 function power(x,y){
     return Math.pow(x,y)
+}
+
+function operation(x,operator,y){
+    // perform operation
+    result = null
+    if(operator === '+')
+        result = add(x,y)
+    else if(operator === '-')
+        result = subtract(x,y)
+    else if(operator === '*')
+        result = multiply(x,y)
+    else if(operator === '/')
+        result = divide(x,y)
+    else if(operator === 'xy')
+        result = power(x,y)
+    console.log(x,operator,y,'=',result);
+    y = null
+    screenPast.textContent = result
+    return result
 }
 
